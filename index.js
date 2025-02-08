193df67ef781d6e6c1d96ff8ae457d7f2cc66491
EduardoBarros00 <lilkank4i20@gmail.com>
2025-02-08 20:06:07 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.once('ready', () => {
+    console.log("✅ Bot está online!");
+});
+
+client.login(process.env.TOKEN);
